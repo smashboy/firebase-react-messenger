82dbb86e8c1feb08dddbfcf45c8194ed404c1e26
Default <sergeitchaban@gmail.com>
2021-08-18 18:57:45 +0300

    Update collections.ts

--- src/core/firebase/collections.ts
+++ src/core/firebase/collections.ts
@@ -1,2 +1,2 @@
-export const CONVERSATION_COLLECTION = "conversations-collection";
-export const MESSAGES_COLLECTION = "messages-collection";
+export const CONVERSATION_COLLECTION = "conversations";
+export const MESSAGES_COLLECTION = "messages";
